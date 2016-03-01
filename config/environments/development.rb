591045a2d96a804c983005b35d5953104a4a0870
Jordandb <Jordandebraekell@hotmail.com>
2016-03-01 09:39:51 +0100

    Configured Devise Gem. Created Devise Views and modified them. Configured ActionMailer

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -36,6 +36,22 @@ Rails.application.configure do
   # Raises helpful error messages.
   config.assets.raise_runtime_errors = true
 
+  # Devise section
+  config.action_mailer.default_options = {from: 'no-reply@projectecommerce.com'}
+  config.action_mailer.default_url_options = { host: 'localhost:3000' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.default :charset => "utf-8"
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.gmail.com',
+    port: 587, 
+    domain: 'gmail.com',
+    user_name: 'jordandebraekell@gmail.com',
+    password: 'Lqtfcp...',
+    authentication: 'plain',
+    enable_starttls_auto: true
+  }
+
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 end
